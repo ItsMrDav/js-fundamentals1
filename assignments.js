f9ab453a3c338c0fff28de7499c670655823008c
Davut Simsek <davutt.simsek@gmail.com>
2025-01-04 18:22:19 +0100

    first commit

---
+++ assignments.js
@@ -0,0 +1,17 @@
+// ***********VALUES AND VARIABLE************************
+
+// let country = `Spain`;
+// let continent = `Europe`;
+// let population = 47.5;
+
+// console.log(`The Country is ${country}, in ${continent} and, has ${population}million population.`);
+
+// ***********DATA TYPES************************
+
+// let isIsland = false;
+// let language;
+
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
---
+++ script.js
@@ -0,0 +1,55 @@
+// let js = `amazing`;
+
+// console.log(40 + 8 + 23 - 10);
+
+// console.log(`Davut`);
+// console.log(23);
+
+// let firstName = `Matilda`;
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// // Variable name conventions
+// let davut_matilda = `DM`;
+// let $function = 32;
+
+// let person = `davut`;
+
+// const PI = 3.1415;
+
+// let myFirstJob = `Coder`;
+// let myCurrentJob = `Teacher`;
+
+// let job1 = `Programmer`;
+// let job2 = `Teacher`;
+
+// console.log(myFirstJob);
+
+// *************DATA TYPES***************
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// // console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// // console.log(typeof 23);
+// // console.log(typeof `Davut`);
+
+// javascriptIsFun = `YES`;
+// console.log(typeof javascriptIsFun);
+
+// //Dynamic typing example
+// let year;
+// console.log(year);
+// console.log(typeof year);
+// year = 1991;
+// console.log(typeof year);
+
+// //JS bug showing null as an 'object' with typeof operator BEWARE of this bug didnt get fixed yet
+// console.log(typeof null);
+
+
+// ************* LET - CONST - VAR ***************
+
