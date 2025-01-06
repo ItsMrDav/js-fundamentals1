607ec1705c334fc56ae3e93ed7fd5770a0a7db6a
Davut Simsek <davutt.simsek@gmail.com>
2025-01-06 17:44:33 +0100

    did more conditional statements

--- assignments.js
+++ assignments.js
@@ -1,9 +1,10 @@
 // *********** VALUES AND VARIABLE ************************
 
-let country = `Spain`;
-let continent = `Europe`;
-let population = 47.5;
-let language = `Spanish`;
+// let country = `Spain`;
+// let continent = `Europe`;
+// let population = 47.5;
+// let language = `Spanish`;
+// let isIsland = false;
 
 // console.log(`The Country is ${country}, in ${continent} and, has ${population}million population.`);
 
@@ -37,7 +38,7 @@ let language = `Spanish`;
 // console.log(`Is ${country} has more population than average=${population > populationAverageCountry}.`);
 // console.log(`${country} is in ${continent}, and its ${population} million people speak ${language}.`)
 
-// ************* CODING CHALLANGE #1  ***************
+// ************* CODING CHALLENGE #1  ***************
 
 // Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula:
 // BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter).
@@ -79,7 +80,7 @@ let language = `Spanish`;
 //     console.log(`${country}'s population has ${population - 33} million below average.`);
 // }
 
-// ************* CODING CHALLANGE #2  ***************
+// ************* CODING CHALLENGE #2  ***************
 
 // if (markBMI > johnBMI) {
 //     console.log(`Mark's BMI (${markBMI}) is higher than John's BMI (${johnBMI}).`);
@@ -88,3 +89,94 @@ let language = `Spanish`;
 // }
 
 // ************* TYPE CONVERSION & TYPE COERCION  ***************
+
+// console.log('9' - '5'); // -> ?4 correct
+// console.log('19' - '13' + '17'); // -> ?617 correct
+// console.log('19' - '13' + 17); // -> ?23 correct
+// console.log('123' < 57); // -> ?false correct
+// console.log(5 + 6 + '4' + 9 - 4 - 2); // -> ?1149->1143 correct
+
+
+// ************* EQUALITY OPERATORS: === VS ==  ***************
+
+// const numNeighbours = Number(prompt(`How many neighbour countries does your country have?`));
+
+// if (numNeighbours === 1) {
+//     console.log(`Only 1 border.`);
+// } else if (numNeighbours > 1) {
+//     console.log(`More than 1 neighbour.`)
+// } else {
+//     console.log(`No borders.`)
+// }
+
+// ************* LOGICAL OPERATORS ***************
+
+// if (language === `English` && population < 50 && !isIsland) {
+//     console.log(`You should live in ${country} :)`);
+// } else {
+//     console.log(`${country} does not meet your criteria :(`);
+// }
+
+// ************* CODING CHALLENGE 4 ***************
+
+// const dolphinScore1 = 97;
+// const dolphinScore2 = 112;
+// const dolphinScore3 = 101;
+// const koolasScore1 = 109;
+// const koolasScore2 = 95;
+// const koolasScore3 = 106;
+
+// const dolphinScoreAverage = (dolphinScore1 + dolphinScore2 + dolphinScore3) / 3;
+// const koolasScoreAverage = (koolasScore1 + koolasScore2 + koolasScore3) / 3;
+
+// console.log(dolphinScoreAverage, koolasScoreAverage);
+
+// if (dolphinScoreAverage > koolasScoreAverage && dolphinScoreAverage >= 100) {
+//     console.log(`Dolphins win ${dolphinScoreAverage} vs ${koolasScoreAverage}`);
+// } else if (koolasScoreAverage > dolphinScoreAverage && koolasScoreAverage >= 100) {
+//     console.log(`Koolas win ${koolasScoreAverage} vs ${dolphinScoreAverage}`);
+// } else if (dolphinScoreAverage === koolasScoreAverage && dolphinScoreAverage >= 100) {
+//     console.log(`That's a tie...`);
+// } else {
+//     console.log(`No team has enough points to win...`)
+// }
+
+//Data 1: No team has enough points to win...
+//Data 2: Koolas win 109 vs 103
+//Data 3: That's a tie...
+
+// ************* SWITCH STATAMENT ***************
+
+// language = `arabic`;
+
+// switch (language) {
+//     case `chinese`:
+//     case `mandarin`:
+//         console.log(`${language} is MOST number of native speakers!`);
+//         break;
+//     case `spanish`:
+//         console.log(`${language} is 2nd place in number of native speakers`);
+//         break;
+//     case `english`:
+//         console.log(`${language} is 3rd place`);
+//         break;
+//     case `hindi`:
+//         console.log(`${language} is number 4`);
+//         break;
+//     case `arabic`:
+//         console.log(`${language} is 5th most spoken language`);
+//         break;
+//     default:
+//         console.log(`${language} is not in top 5 most spoken language`);
+// }
+
+// ************* TERNARY OPERATOR ***************
+
+// console.log(`${country}'s population is ${population >= 33 ? `over` : `below`} average.`);
+
+// ************* CODING CHALLENGE #4 ***************
+
+// const bill = 430;
+
+// const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+// console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${bill + tip}.`);
--- script.js
+++ script.js
@@ -141,3 +141,178 @@
 // console.log(century);
 
 // ************* TYPE CONVERSION & TYPE COERCION  ***************
+
+// type conversion-willingly-explicityly
+// const inputYear = `1992`;
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
+
+// console.log(Number(`Davut`));
+// console.log(typeof NaN);
+
+// console.log(String(23), 23);
+
+// type coercion-unwillingly-implicitly
+// console.log(`I am ` + 23 + ` years old`);
+// console.log(`I am ` + `23` + ` years old`);
+// console.log(`I am ` + String(23) + ` years old`);
+
+// console.log(`23` + `10` + 3);
+// console.log(`23` - `10` - 3);
+// console.log(`23` / `10` / 3);
+// console.log(`23` * `10` * 3);
+
+// let n = `1` + 1;
+// n--;
+// console.log(n);
+
+// ************* TRUTHY & FALSY VALUES  ***************
+
+// There are 5 falsy values(0, ``, undefined, null, NaN)
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean(`Davut`));
+// console.log(Boolean({}));
+// console.log(Boolean(``));
+// console.log(Boolean(''));
+// console.log(Boolean(""));
+// console.log(Boolean(null));
+// console.log(Boolean(NaN));
+
+// const money = 111;
+// if (money) {
+//     console.log(`Don't spend it all ;)`);
+// } else {
+//     console.log(`You should get a job...`);
+// }
+
+// // let height = 0;
+// // if (height) {
+// //     console.log(`YAY! Height is defined`);
+// // } else {
+// //     console.log(`Height is undefined...`);
+// // }
+
+
+// let height = 0;
+// if (height === 0 || height) {
+//     console.log(`YAY! Height is defined`);
+// } else {
+//     console.log(`Height is undefined...`);
+// }
+
+// ************* EQUALITY OPERATORS: === VS ==  ***************
+
+// const age = `18`;
+// if (age === 18) console.log(`You just became an adult (strict)`);
+// if (age == 18) console.log(`You just became an adult (loose)`);
+
+// const favorite = Number(prompt(`What's your favorite number?`));
+// console.log(favorite);
+// console.log(typeof favorite);
+
+// if (favorite === 23) {
+//     console.log(`Cool! 23 is an amazing number`);
+// } else if (favorite === 7) {
+//     console.log(`7 is also a cool number.`)
+// } else if (favorite === 9) {
+//     console.log(`9 is also a good number.`)
+// } else {
+//     console.log(`Number is not 23 or 7 or 9.`)
+// }
+
+// if (favorite !== 23) console.log(`Why not 23?`);
+
+// ************* LOGICAL OPERATORS ***************
+
+// const hasDriverLicence = true;
+// const hasGoodVision = true;
+
+// console.log(hasDriverLicence && hasGoodVision);
+// console.log(hasDriverLicence || hasGoodVision);
+// console.log(!hasDriverLicence);
+
+
+// // if (hasDriverLicence && hasGoodVision) {
+// //     console.log(`Sarah is able to drive!`);
+// // } else {
+// //     console.log(`Someone else should drive...`)
+// // }
+
+// const isTired = false;
+
+// if (hasDriverLicence && hasGoodVision && !isTired) {
+//     console.log(`Sarah is able to drive!`);
+// } else {
+//     console.log(`Someone else should drive...`)
+// }
+
+// ************* SWITCH STATAMENT ***************
+
+// const day = `thursday`;
+
+// switch (day) {
+//     case `monday`:
+//         console.log(`Plan course structure`);
+//         console.log(`Go to coding meetup`);
+//         break;
+//     case `tuesday`:
+//         console.log(`Prepare theory videos`)
+//         break;
+//     case `wednesday`:
+//     case `thursday`:
+//         console.log(`Write code examples`);
+//         break;
+//     case `friday`:
+//         console.log(`Record videos`);
+//         break;
+//     case `saturday`:
+//     case `sunday`:
+//         console.log(`Enjoy the weekend`);
+//         break;
+//     default:
+//         console.log(`Not a valid day!`);
+// }
+
+// if (day === `wednesday` || day === `thursday`) {
+//     console.log(`Correct answer...`);
+// }
+
+// ************* STATEMENTS & EXPRESSIONS ***************
+
+// //Expression produces a VALUE((part of sentence)
+// //Statement does not produce a value but ACTION(a sentence), it and with semicolon(;)
+
+// // Expression Examples
+// 3 + 4
+// 1992
+// true && false && !false
+
+// // Statement Examples
+// if (23 > 10) {
+//     const str = `23 is bigger`; //here only a part of this code is expression 
+// }
+// console.log(`I'm ${2037 - 1992} years old.`);
+
+// ************* TERNARY OPERATOR ***************
+
+// const age = 15;
+// age > 18 ? console.log(`I'd like to drink beer.`) : null;
+// // age > 18 && console.log(`I'd like to drink beer.`); // shor circuit doesn't require false condition
+// age > 20 ? console.log(`I'd like to drink wine also.`) : console.log(`I'd like to drink water.`);
+
+// const drink = age === 18 ? `beer` : age >= 19 ? `wine` : `water`;
+// console.log(drink);
+
+// let drink2;
+// if (age >= 18) {
+//     drink2 = `wine`;
+// } else {
+//     drink2 = `water`;
+// }
+// console.log(drink2);
+
+
+// // Ternary operator can be used in template literal. because is considered a expression
+// console.log(`I'd like to drink ${age >= 18 ? `wine` : `water`}`);
